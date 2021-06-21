84baea71beb3d60058ff188cddddf83fcc7df30d
Michael Bechtel <mbechtel@ku.edu>
2021-06-21 11:42:59 -0500

    Add separate script for bandwidth-rt co-runners

--- scripts/bw_vs_bwrt.sh
+++ scripts/bw_vs_bwrt.sh
@@ -53,8 +53,8 @@ echo "$bw, $l1miss, $misses, $loads, $missrate"
 for ((i=1; i<4; i+=1))
 #for i in 1 2 3 5 6 7
 do
-	bandwidth -c $i -t 0 -a $cacc -m $csize $alloc &> /dev/null &
-	#bandwidth-rt -c 1 -n $i -t 0 -a $cacc -m $csize $alloc $buffer &> /dev/null &
+	#bandwidth -c $i -t 0 -a $cacc -m $csize $alloc &> /dev/null &
+	bandwidth-rt -c 1 -n $i -t 0 -a $cacc -m $csize $alloc $buffer &> /dev/null &
 	output=`perf stat -e $count chrt -f 1 bandwidth -c 0 -i $viter -t 0 -m $vsize -a $vacc $alloc 2>&1` # 2> /dev/null | grep average | awk 'NF{ print $(NF-7) }'`
 	bw=`echo "$output" | grep average | awk 'NF{ print $(NF-7) }'`
 	l1miss=`echo "$output" | grep "L1-dcache-load-misses" | awk '{ print $1 }' | sed 's/,//g'` 
@@ -63,7 +63,7 @@ do
 	missrate=`echo "$output" | grep "LLC-load-misses" | awk 'NF{ print $(NF-4) }' | sed 's/%//g'`
 	echo "$bw, $l1miss, $misses, $loads, $missrate"
 	#echo "$output"
-	#killall bandwidth-rt
+	killall bandwidth-rt
 done
-killall bandwidth
+#killall bandwidth
 
