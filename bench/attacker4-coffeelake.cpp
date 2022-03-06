4860524964e01260de6ac6ee018c5bc15a5a37f0
Heechul Yun <heechul.yun@ku.edu>
2022-03-06 16:59:38 -0600

    gpu version working. is it better attacker?
    yes, it is a much better attacker than the original gpuwrite.
    
    observe more than >400x slowdown
    
    gpu attacker
    $ sudo ./attacker4-coffeelake -m 524288  -l 128 -e 0 -i 1000 &
    
    cpu victim
    
    heechul@coffee:~/Projects/IsolBench/bench$ sudo ./bandwidth -c 0 -m 2048
    -t 1 -x
    assigned to cpu 0
    memsize=2048 KB, type=read, cpuid=0
    stop at 1
    g_nread(bytes read) = 285212672
    elapsed = 1.00 sec ( 1000064 usec )
    CPU0: B/W = 271.98 MB/s | CPU0: average = 224.41 ns
    					  ^^^^^^      >200X slowdown
    
    heechul@coffee:~/Projects/IsolBench/bench$ sudo ./bandwidth -c 0 -m
    32768 -t 1 -x
    assigned to cpu 0
    memsize=32768 KB, type=read, cpuid=0
    stop at 1
    g_nread(bytes read) = 100663296
    elapsed = 1.00 sec ( 999993 usec )
    CPU0: B/W = 96.00 MB/s | CPU0: average = 635.78 ns
    					 ^^^^^^^^^	>400X slowdown

--- bench/attacker4-coffeelake.cpp
+++ bench/attacker4-coffeelake.cpp
@@ -6,6 +6,8 @@
  * This file is distributed under the University of Illinois Open Source
  * License. See LICENSE.TXT for details.
  *
+ * Usage:
+ *   $ sudo ./attacker4-coffeelake -m 65536 -l 12 
  */ 
 
 /**************************************************************************
@@ -37,24 +39,16 @@
 #include <sys/resource.h>
 #include <assert.h>
 
+#include "cl2.hpp"
+
 /**************************************************************************
  * Public Definitions
  **************************************************************************/
-#define MAX_MLP 32
+#define MAX_MLP 256
 #define CACHE_LINE_SIZE 64
-#ifdef __arm__
-#  define DEFAULT_ALLOC_SIZE_KB 4096
-#else
-#  define DEFAULT_ALLOC_SIZE_KB 16384
-#endif
+#define DEFAULT_ALLOC_SIZE_KB 65536
 #define DEFAULT_ITER 100
 
-#ifdef __LP64__
-#define BITS_PER_LONG 64
-#else
-#define BITS_PER_LONG 32
-#endif
-#define BITOP_WORD(nr)		((nr) / BITS_PER_LONG)
 #define PAGE_SHIFT 12
 
 #define MAX_COLORS 64
@@ -69,7 +63,7 @@ enum access_type { READ, WRITE};
  **************************************************************************/
 static int g_mem_size = (DEFAULT_ALLOC_SIZE_KB*1024);
 static int* list[MAX_MLP];
-static int next[MAX_MLP];
+static int* next;
 
 static int g_debug = 0;
 static int g_color[MAX_COLORS]; // not assigned
@@ -112,6 +106,31 @@ int paddr_to_color(unsigned long addr)
 		(bit(addr,15)^bit(addr,18))<<1|
 		(bit(addr,16)^bit(addr,19)));
 }
+
+// ---------------------------------------------------------------------------
+size_t libkdump_virt_to_phys(size_t virtual_address) {
+  static int pagemap = -1;
+  if (pagemap == -1) {
+    pagemap = open("/proc/self/pagemap", O_RDONLY);
+    if (pagemap < 0) {
+      errno = EPERM;
+      return 0;
+    }
+  }
+  uint64_t value;
+  int got = pread(pagemap, &value, 8, (virtual_address / 0x1000) * 8);
+  if (got != 8) {
+    errno = EPERM;
+    return 0;
+  }
+  uint64_t page_frame_number = value & ((1ULL << 54) - 1);
+  if (page_frame_number == 0) {
+    errno = EPERM;
+    return 0;
+  }
+  return page_frame_number * 0x1000 + virtual_address % 0x1000;
+}
+
 
 /**************************************************************************
  * Implementation
@@ -381,7 +400,6 @@ int main(int argc, char* argv[])
 	}
 
 	printf("sizeof(unsigned long): %d\n", (int)sizeof(unsigned long));
-	printf("BITS_PER_LONG: %d\n", BITS_PER_LONG);
 
 	printf("\n");
 	if (g_color_cnt) {
@@ -402,6 +420,7 @@ int main(int argc, char* argv[])
 	clock_gettime(CLOCK_REALTIME, &start);
 
 	/* alloc memory. align to a page boundary */
+#if 0
 	if (use_hugepage) {
 		memchunk = (int *)mmap(0, 
 				       g_mem_size,
@@ -420,22 +439,115 @@ int main(int argc, char* argv[])
 		}
 		printf("Using malloc(), not very accurate\n");
 	}
+#else
 
+	// Get platform and device information
+	cl_platform_id platform_id = NULL;
+	cl_device_id device_id = NULL;   
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_device_svm_capabilities caps;
+	cl_int ret, err;
+
+	err = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	if (ret_num_platforms == 0) {
+		std::cerr<<" No platforms found. Check OpenCL installation!\n";
+		exit(1);
+	}
+
+	err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, 
+			     &device_id, &ret_num_devices);
+	if (ret_num_devices == 0) {
+		std::cerr<<" No devices found. Check OpenCL installation!\n";
+		exit(1);
+	}
+
+	// Create an OpenCL context
+	cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+
+	// print device name
+	size_t valueSize;
+	clGetDeviceInfo(device_id, CL_DEVICE_NAME, 0, NULL, &valueSize);
+	char *value = (char*) malloc(valueSize);
+	clGetDeviceInfo(device_id, CL_DEVICE_NAME, valueSize, value, NULL);
+	printf("%d. Device: %s\n", 1, value);
+	free(value);
+
+	// print hardware device version
+	clGetDeviceInfo(device_id, CL_DEVICE_VERSION, 0, NULL, &valueSize);
+	value = (char*) malloc(valueSize);
+	clGetDeviceInfo(device_id, CL_DEVICE_VERSION, valueSize, value, NULL);
+	printf(" %d.%d Hardware version: %s\n", 1, 1, value);
+	free(value);
+
+	// print software driver version
+	clGetDeviceInfo(device_id, CL_DRIVER_VERSION, 0, NULL, &valueSize);
+	value = (char*) malloc(valueSize);
+	clGetDeviceInfo(device_id, CL_DRIVER_VERSION, valueSize, value, NULL);
+	printf(" %d.%d Software version: %s\n", 1, 2, value);
+	free(value);
+
+	// print c version supported by compiler for device
+	clGetDeviceInfo(device_id, CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+	value = (char*) malloc(valueSize);
+	clGetDeviceInfo(device_id, CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+	printf(" %d.%d OpenCL C version: %s\n", 1, 3, value);
+	free(value);
+
+	// print parallel compute units
+	cl_uint maxComputeUnits;
+	clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS,
+			sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+	printf(" %d.%d Parallel compute units: %d\n", 1, 4, maxComputeUnits);
+
+	err = clGetDeviceInfo(device_id,
+				     CL_DEVICE_SVM_CAPABILITIES,
+				     sizeof(cl_device_svm_capabilities),
+				     &caps,
+				     0);
+	printf(" 1.4 SVM support:");
+	if (caps & CL_DEVICE_SVM_COARSE_GRAIN_BUFFER)
+		printf(" CL_DEVICE_SVM_COARSE_GRAIN");
+	if (caps & CL_DEVICE_SVM_FINE_GRAIN_BUFFER)
+		printf(" CL_DEVICE_SVM_FINE_GRAIN_BUFFER");
+	if (caps & CL_DEVICE_SVM_FINE_GRAIN_SYSTEM)
+		printf(" CL_DEVICE_SVM_FINE_GRAIN_SYSTEM");
+	if (caps & CL_DEVICE_SVM_ATOMICS)
+		printf(" CL_DEVICE_SVM_ATOMICS");
+	printf("\n");
+	
+	// Create a command queue
+	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
+
+
+	memchunk = (int *)clSVMAlloc(context, CL_MEM_READ_WRITE, g_mem_size, 0);
+	next = (int *)clSVMAlloc(context, CL_MEM_READ_WRITE, MAX_MLP * sizeof(int), 0);
+#endif
+	
 	/* initialize data */
 	memset(memchunk, 0, g_mem_size);
 
 	// set some values:
-	for (int i=0; i<orig_ws; i++) {
-		ulong vaddr = (ulong)&memchunk[i*CACHE_LINE_SIZE/4];
+	int page_size = 1<<PAGE_SHIFT;
 
+	ulong vaddr = 0;
+	ulong paddr = 0;
+	for (int i=0; i<orig_ws; i++) {
+		vaddr = (ulong)&memchunk[i*CACHE_LINE_SIZE/4];
+		if (i % (page_size/CACHE_LINE_SIZE) == 0) {
+			paddr = (ulong)libkdump_virt_to_phys(vaddr);
+
+			if (g_debug)
+				printf("vaddr: %p padddr: %p color: %d\n",
+				       (void *)vaddr, (void *)paddr,
+				       paddr_to_color(paddr));
+		} else
+			paddr = paddr + CACHE_LINE_SIZE;
+		
 		if (g_color_cnt > 0) {
 			/* use coloring */
 			for (int j = 0; j < g_color_cnt; j++) {
-				if (paddr_to_color(vaddr) == g_color[j]) {
-					if (g_debug)
-						printf("vaddr: %p color: %d\n",
-						       (void *)vaddr,
-						       paddr_to_color(vaddr));
+				if (paddr_to_color(paddr) == g_color[j]) {
 					myvector.push_back(i);
 				}
 			}
@@ -451,16 +563,16 @@ int main(int argc, char* argv[])
 
 	// update the workingset size
 	ws = myvector.size() / mlp * mlp; 
+
 	printf("new ws: %d\n", ws);
 	int list_len = ws / mlp;
-	printf("list_len: %d\n", list_len);
 	
 	for (i = 0; i < ws; i++) {
 		int l = i / list_len;
 		int curr_idx = myvector[i] * CACHE_LINE_SIZE / 4;
 		int next_idx = myvector[i+1] * CACHE_LINE_SIZE / 4;
 
-#if 0		
+#if 1
 		if ((i+1) % list_len == 0)
 			next_idx = myvector[i/list_len*list_len] * CACHE_LINE_SIZE / 4;
 #else
@@ -473,7 +585,7 @@ int main(int argc, char* argv[])
 		if (i % list_len == 0) {
 			list[l] = memchunk;
 			next[l] = curr_idx;
-			printf("list[%d]  %d\n", l,  next[l] * 4 / CACHE_LINE_SIZE);
+			printf("next[%d]  %d\n", l,  next[l]);
 		}
 		
 		// printf("%8d ->%8d\n", myvector[i], next_idx*4/CACHE_LINE_SIZE);
@@ -491,9 +603,12 @@ int main(int argc, char* argv[])
 	clock_gettime(CLOCK_REALTIME, &end);
 	printf("Init took %.0f us\n", (double) get_elapsed(&start, &end)/1000);
 
-
 	long naccess;
-	clock_gettime(CLOCK_REALTIME, &start);
+	int64_t nsdiff;
+	double  avglat;
+	int total_ws;
+#if 0
+	clock_gettime(CLOCK_REALTIME, &start);	
 	/* actual access */
 	if (acc_type == READ)
 		naccess = run((int64_t)repeat * list_len, mlp);
@@ -501,15 +616,73 @@ int main(int argc, char* argv[])
 		naccess = run_write((int64_t)repeat * list_len, mlp);
 	clock_gettime(CLOCK_REALTIME, &end);
 
-	int64_t nsdiff = get_elapsed(&start, &end);
-	double  avglat = (double)nsdiff/naccess;
+	nsdiff = get_elapsed(&start, &end);
+	avglat = (double)nsdiff/naccess;
 
 	printf("alloc. size: %d (%d KB)\n", g_mem_size, g_mem_size/1024);
-	int total_ws =  ws * CACHE_LINE_SIZE;
+	total_ws =  ws * CACHE_LINE_SIZE;
 	printf("ws size: %d (%d KB)\n", total_ws, total_ws / 1024);
+	printf("list_len: %d (%d KB)\n", list_len, list_len * CACHE_LINE_SIZE / 1024);
+	printf("mlp: %d\n", mlp);
 	printf("duration %.0f ns, #access %ld\n", (double)nsdiff, naccess);
 	printf("Avg. latency %.2f ns\n", avglat);	
 	printf("bandwidth %.2f MB/s\n", (double)64*1000*naccess/nsdiff);
+#endif
 
+
+	// Load the kernel source code into the array source_str
+	FILE *fp;
+	char *source_str;
+	size_t source_size;
+	
+	fp = fopen("gpuwrite.cl", "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
+	#define MAX_SOURCE_SIZE (0x100000)
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose( fp );
+ 	
+	cl_program program = clCreateProgramWithSource(context, 1,
+						       (const char **)&source_str, (const size_t *)&source_size, &ret); 
+	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	cl_kernel kernel = clCreateKernel(program, "gpuwrite", &ret);
+	clSetKernelArgSVMPointer(kernel, 0, memchunk);
+	clSetKernelArgSVMPointer(kernel, 1, next);
+	int iter = repeat * list_len; 
+	clSetKernelArg(kernel, 2, sizeof(int), &iter);
+	
+	size_t global_item_size = mlp;
+	size_t local_item_size = 1;
+
+	clock_gettime(CLOCK_REALTIME, &start);
+	
+	clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, NULL, 0, NULL, NULL);
+	ret = clFlush(command_queue);
+	ret = clFinish(command_queue);
+	
+	clock_gettime(CLOCK_REALTIME, &end);
+
+
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+
+	naccess = global_item_size * list_len * repeat;
+
+
+	nsdiff = get_elapsed(&start, &end);
+	avglat = (double)nsdiff/naccess;
+
+	printf("alloc. size: %d (%d KB)\n", g_mem_size, g_mem_size/1024);
+	total_ws =  ws * CACHE_LINE_SIZE;
+	printf("ws size: %d (%d KB)\n", total_ws, total_ws / 1024);
+	printf("list_len: %d (%d KB)\n", list_len, list_len * CACHE_LINE_SIZE / 1024);
+	printf("mlp: %d\n", mlp);
+	printf("duration %.0f ns, #access %ld\n", (double)nsdiff, naccess);
+	printf("Avg. latency %.2f ns\n", avglat);	
+	printf("bandwidth %.2f MB/s\n", (double)64*1000*naccess/nsdiff);
+	
 	return 0;
 }
--- bench/ocl_simple_add.cpp
+++ bench/ocl_simple_add.cpp
@@ -3,7 +3,8 @@
 // #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 // #define CL_HPP_TARGET_OPENCL_VERSION 120
 #include "cl2.hpp"
- 
+// #include <CL/cl.h>
+
 int main(){
     //get all platforms (drivers)
     std::vector<cl::Platform> all_platforms;
