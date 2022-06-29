acec1df1abc7922c5d78cc9035d4d47c6b8f1fbe
Heechul Yun <heechul.yun@ku.edu>
2022-06-29 17:44:58 -0500

    cleanup

--- bench/attacker4-coffeelake.cpp
+++ bench/attacker4-coffeelake.cpp
@@ -14,6 +14,8 @@
  * Conditional Compilation Options
  **************************************************************************/
 
+#pragma OPENCL EXTENSION cl_intel_printf : enable
+
 /**************************************************************************
  * Included Files
  **************************************************************************/
@@ -62,7 +64,6 @@ enum access_type { READ, WRITE};
  * Global Variables
  **************************************************************************/
 static int g_mem_size = (DEFAULT_ALLOC_SIZE_KB*1024);
-static int* list[MAX_MLP];
 static int* next;
 
 static int g_debug = 0;
@@ -135,196 +136,11 @@ size_t libkdump_virt_to_phys(size_t virtual_address) {
 /**************************************************************************
  * Implementation
  **************************************************************************/
-int64_t run(int64_t iter, int mlp)
-{
-	int64_t cnt = 0;
-
-	for (int64_t i = 0; i < iter; i++) {
-		switch (mlp) {
-		case 32:
-			next[31] = list[31][next[31]];
-		case 31:
-			next[30] = list[30][next[30]];
-		case 30:
-			next[29] = list[29][next[29]];
-		case 29:
-			next[28] = list[28][next[28]];
-		case 28:
-			next[27] = list[27][next[27]];
-		case 27:
-			next[26] = list[26][next[26]];
-		case 26:
-			next[25] = list[25][next[25]];
-		case 25:
-			next[24] = list[24][next[24]];
-		case 24:
-			next[23] = list[23][next[23]];
-		case 23:
-			next[22] = list[22][next[22]];
-		case 22:
-			next[21] = list[21][next[21]];
-		case 21:
-			next[20] = list[20][next[20]];
-		case 20:
-			next[19] = list[19][next[19]];
-		case 19:
-			next[18] = list[18][next[18]];
-		case 18:
-			next[17] = list[17][next[17]];
-		case 17:
-			next[16] = list[16][next[16]];
-		case 16:
-			next[15] = list[15][next[15]];
-		case 15:
-			next[14] = list[14][next[14]];
-		case 14:
-			next[13] = list[13][next[13]];
-		case 13:
-			next[12] = list[12][next[12]];
-		case 12:
-			next[11] = list[11][next[11]];
-		case 11:
-			next[10] = list[10][next[10]];
-		case 10:
-			next[9] = list[9][next[9]];
-		case 9:
-			next[8] = list[8][next[8]];
-		case 8:
-			next[7] = list[7][next[7]];
-		case 7:
-			next[6] = list[6][next[6]];
-		case 6:
-			next[5] = list[5][next[5]];
-		case 5:
-			next[4] = list[4][next[4]];
-		case 4:
-			next[3] = list[3][next[3]];
-		case 3:
-			next[2] = list[2][next[2]];
-		case 2:
-			next[1] = list[1][next[1]];
-		case 1:
-			next[0] = list[0][next[0]];
-		}
-		cnt += mlp;
-
-	}
-	return cnt;
-}
-
-long run_write(long iter, int mlp)
-{
-	long cnt = 0;
-
-	for (long i = 0; i < iter; i++) {
-		switch (mlp) {
-		case 32:
-			list[31][next[31]+1] = 0xff;
-			next[31] = list[31][next[31]];
-		case 31:
-			list[30][next[30]+1] = 0xff;
-			next[30] = list[30][next[30]];
-		case 30:
-			list[29][next[29]+1] = 0xff;
-			next[29] = list[29][next[29]];
-		case 29:
-			list[28][next[28]+1] = 0xff;
-			next[28] = list[28][next[28]];
-		case 28:
-			list[27][next[27]+1] = 0xff;
-			next[27] = list[27][next[27]];
-		case 27:
-			list[26][next[26]+1] = 0xff;
-			next[26] = list[26][next[26]];
-		case 26:
-			list[25][next[25]+1] = 0xff;
-			next[25] = list[25][next[25]];
-		case 25:
-			list[24][next[24]+1] = 0xff;
-			next[24] = list[24][next[24]];
-		case 24:
-			list[23][next[23]+1] = 0xff;
-			next[23] = list[23][next[23]];
-		case 23:
-			list[22][next[22]+1] = 0xff;
-			next[22] = list[22][next[22]];
-		case 22:
-			list[21][next[21]+1] = 0xff;
-			next[21] = list[21][next[21]];
-		case 21:
-			list[20][next[20]+1] = 0xff;
-			next[20] = list[20][next[20]];
-		case 20:
-			list[19][next[19]+1] = 0xff;
-			next[19] = list[19][next[19]];
-		case 19:
-			list[18][next[18]+1] = 0xff;
-			next[18] = list[18][next[18]];
-		case 18:
-			list[17][next[17]+1] = 0xff;
-			next[17] = list[17][next[17]];
-		case 17:
-			list[16][next[16]+1] = 0xff;
-			next[16] = list[16][next[16]];
-		case 16:
-			list[15][next[15]+1] = 0xff;
-			next[15] = list[15][next[15]];
-		case 15:
-			list[14][next[14]+1] = 0xff;
-			next[14] = list[14][next[14]];
-		case 14:
-			list[13][next[13]+1] = 0xff;
-			next[13] = list[13][next[13]];
-		case 13:
-			list[12][next[12]+1] = 0xff;
-			next[12] = list[12][next[12]];
-		case 12:
-			list[11][next[11]+1] = 0xff;
-			next[11] = list[11][next[11]];
-		case 11:
-			list[10][next[10]+1] = 0xff;
-			next[10] = list[10][next[10]];
-		case 10:
-			list[9][next[9]+1] = 0xff;
-			next[9] = list[9][next[9]];
-		case 9:
-			list[8][next[8]+1] = 0xff;
-			next[8] = list[8][next[8]];
-		case 8:
-			list[7][next[7]+1] = 0xff;
-			next[7] = list[7][next[7]];
-		case 7:
-			list[6][next[6]+1] = 0xff;
-			next[6] = list[6][next[6]];
-		case 6:
-			list[5][next[5]+1] = 0xff;
-			next[5] = list[5][next[5]];
-		case 5:
-			list[4][next[4]+1] = 0xff;
-			next[4] = list[4][next[4]];
-		case 4:
-			list[3][next[3]+1] = 0xff;
-			next[3] = list[3][next[3]];
-		case 3:
-			list[2][next[2]+1] = 0xff;
-			next[2] = list[2][next[2]];
-		case 2:
-			list[1][next[1]+1] = 0xff;
-			next[1] = list[1][next[1]];
-		case 1:
-			list[0][next[0]+1] = 0xff;
-			next[0] = list[0][next[0]];
-		}
-		cnt += mlp;
-
-	}
-	return cnt;
-}
 
 
 int main(int argc, char* argv[])
 {
-	// struct sched_param param;
+	struct sched_param param;
         cpu_set_t cmask;
 	int num_processors;
 	int cpuid = 0;
@@ -335,7 +151,6 @@ int main(int argc, char* argv[])
 
 	long repeat = DEFAULT_ITER;
 	int mlp = 1;
-	int use_hugepage = 0;
 	struct timespec start, end;
 	int acc_type = READ;
 
@@ -392,9 +207,6 @@ int main(int argc, char* argv[])
 			mlp = strtol(optarg, NULL, 0);
 			fprintf(stderr, "MLP=%d\n", mlp);
 			break;
-                case 'x':
-			use_hugepage = (use_hugepage) ? 0: 1;
-			break;
 		}
 
 	}
@@ -420,27 +232,6 @@ int main(int argc, char* argv[])
 	clock_gettime(CLOCK_REALTIME, &start);
 
 	/* alloc memory. align to a page boundary */
-#if 0
-	if (use_hugepage) {
-		memchunk = (int *)mmap(0, 
-				       g_mem_size,
-				       PROT_READ | PROT_WRITE, 
-				       MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, 
-				       -1, 0);
-		if ((void *)memchunk == MAP_FAILED) {
-			perror("alloc failed");
-			exit(1);
-		}
-	} else {
-		memchunk = (int *)malloc(g_mem_size);
-		if (memchunk == NULL) {
-			perror("alloc failed");
-			exit(1);
-		}
-		printf("Using malloc(), not very accurate\n");
-	}
-#else
-
 	// Get platform and device information
 	cl_platform_id platform_id = NULL;
 	cl_device_id device_id = NULL;   
@@ -522,7 +313,6 @@ int main(int argc, char* argv[])
 
 	memchunk = (int *)clSVMAlloc(context, CL_MEM_READ_WRITE, g_mem_size, 0);
 	next = (int *)clSVMAlloc(context, CL_MEM_READ_WRITE, MAX_MLP * sizeof(int), 0);
-#endif
 	
 	/* initialize data */
 	memset(memchunk, 0, g_mem_size);
@@ -571,34 +361,22 @@ int main(int argc, char* argv[])
 		int l = i / list_len;
 		int curr_idx = myvector[i] * CACHE_LINE_SIZE / 4;
 		int next_idx = myvector[i+1] * CACHE_LINE_SIZE / 4;
-
-#if 1
 		if ((i+1) % list_len == 0)
 			next_idx = myvector[i/list_len*list_len] * CACHE_LINE_SIZE / 4;
-#else
-		if (i + 1 == ws)
-			next_idx = myvector[0] * CACHE_LINE_SIZE / 4;
-#endif		
-		
 		memchunk[curr_idx] = next_idx;
 		
 		if (i % list_len == 0) {
-			list[l] = memchunk;
 			next[l] = curr_idx;
-			printf("next[%d]  %d\n", l,  next[l]);
+			// printf("next[%d]  %d\n", l,  next[l]);
 		}
 		
 		// printf("%8d ->%8d\n", myvector[i], next_idx*4/CACHE_LINE_SIZE);
 	}
 	
-	if (use_hugepage) printf("Using hugetlb\n");
-
-#if 0
         param.sched_priority = 1;
         if(sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
 		perror("sched_setscheduler failed");
         }
-#endif
 
 	clock_gettime(CLOCK_REALTIME, &end);
 	printf("Init took %.0f us\n", (double) get_elapsed(&start, &end)/1000);
@@ -607,28 +385,6 @@ int main(int argc, char* argv[])
 	int64_t nsdiff;
 	double  avglat;
 	int total_ws;
-#if 0
-	clock_gettime(CLOCK_REALTIME, &start);	
-	/* actual access */
-	if (acc_type == READ)
-		naccess = run((int64_t)repeat * list_len, mlp);
-	else
-		naccess = run_write((int64_t)repeat * list_len, mlp);
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	nsdiff = get_elapsed(&start, &end);
-	avglat = (double)nsdiff/naccess;
-
-	printf("alloc. size: %d (%d KB)\n", g_mem_size, g_mem_size/1024);
-	total_ws =  ws * CACHE_LINE_SIZE;
-	printf("ws size: %d (%d KB)\n", total_ws, total_ws / 1024);
-	printf("list_len: %d (%d KB)\n", list_len, list_len * CACHE_LINE_SIZE / 1024);
-	printf("mlp: %d\n", mlp);
-	printf("duration %.0f ns, #access %ld\n", (double)nsdiff, naccess);
-	printf("Avg. latency %.2f ns\n", avglat);	
-	printf("bandwidth %.2f MB/s\n", (double)64*1000*naccess/nsdiff);
-#endif
-
 
 	// Load the kernel source code into the array source_str
 	FILE *fp;
@@ -655,16 +411,19 @@ int main(int argc, char* argv[])
 	clSetKernelArg(kernel, 2, sizeof(int), &iter);
 	
 	size_t global_item_size = mlp;
-	size_t local_item_size = 1;
+	size_t local_item_size = list_len;
 
+	printf("launch the gpu kernel\n");
 	clock_gettime(CLOCK_REALTIME, &start);
 	
-	clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, NULL, 0, NULL, NULL);
+	clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, NULL, 0, NULL,NULL);
+	printf("before flush\n");
 	ret = clFlush(command_queue);
+	printf("before finish\n");
 	ret = clFinish(command_queue);
-	
 	clock_gettime(CLOCK_REALTIME, &end);
-
+	printf("gpu kernel finishes\n");
+	
 
 	ret = clReleaseKernel(kernel);
 	ret = clReleaseProgram(program);
--- bench/gpuwrite.cl
+++ bench/gpuwrite.cl
@@ -1,9 +1,11 @@
 kernel void gpuwrite(__global int* array, __global int *next, int iter)
 {
     const size_t tid = get_global_id(0);
-    printf("next[%u]=%u iter=%u\n", tid, next[tid], iter);
+    // printf("next[%u]=%u iter=%u\n", tid, next[tid], iter);
     for (int r = 0; r < iter; r++) {
         array[next[tid]+1] = 0xff;
+	// if (r % 1000 == 0) printf("r=%d\n", r);
         next[tid] = array[next[tid]];
     }
+
 }
